762c4a191ed5190883941d115fc5f7abc119ff0f
nataliapovaliy <natalia.bg1125@gmail.com>
2023-01-18 21:37:05 +0200

    update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import user from './json/user.json';
+import data from './json/data.json';
+import friends from './json/friends.json';
+import transactions from './json/transactions.json';
+import Profile from "./components/Profile/Profile";
+import Statistics from "./components/Statistics/Statistics";
+import FriendList from './components/FriendList/FriendList';
+
+export const App = () => {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+    <Statistics title="Upload stats" stats={data} />
+    <FriendList friends={friends} />;
+    </>
+  );
+};
