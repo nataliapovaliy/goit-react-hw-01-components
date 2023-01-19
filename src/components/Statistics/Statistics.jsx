4e74d811c5c3578aaa59fa5c8e51155e161a135f
nataliapovaliy <natalia.bg1125@gmail.com>
2023-01-19 18:52:27 +0200

    update Statisticss.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,7 +6,7 @@ export default function Statistics({title, stats}) {
     return (
         <>
         <section className={css.statistics}>
-                <h2 className={css.title}>{title}</h2>
+                {title && <h2 className={css.title}>{title}</h2>}
                 
                 {<StatList stats={stats}/>}
         </section>
