b474b8e5ebcd87f9f8f4d13aff31cefca4eab195
nataliapovaliy <natalia.bg1125@gmail.com>
2023-01-19 09:56:09 +0200

    update without css in transaction.jsx

--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from './TransactionHistory.module.css';
+// import css from './TransactionHistory.module.css';
 
 export const Transaction = ({type, amount, currency}) => {
     return (
